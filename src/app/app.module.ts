56dae74880d8ca877ac8633e08ec5a359518bbe8
Al Zimmerman <auraelius@gmail.com>
2017-06-03 22:51:24 -0700

    Lesson 2 complete - plugins and libraries added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { ConnectivityProvider } from '../providers/connectivity/connectivity';
+import { DataProvider } from '../providers/data/data';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation';
+import { GoogleMapsProvider } from '../providers/google-maps/google-maps';
+
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicStorageModule } from '@ionic/storage';
+import { Keyboard } from '@ionic-native/keyboard';
+import { MyApp } from './app.component';
+import { Network } from '@ionic-native/network';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp
+
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp
+
   ],
   providers: [
-    StatusBar,
+    ConnectivityProvider,
+    DataProvider,
+    Geolocation,
+    GoogleMapsProvider,
+    InAppBrowser,
+    Keyboard,
+    Network,
     SplashScreen,
+    StatusBar,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
---
+++ src/providers/google-maps/google-maps.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the GoogleMapsProvider provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class GoogleMapsProvider {
+
+  constructor() {
+    console.log('Hello GoogleMapsProvider Provider');
+  }
+
+}
